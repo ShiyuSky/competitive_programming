5b408e2e83c409ff755e7ab83e893fc3aede5900
Daniel Liu <danielliu2727@gmail.com>
2021-04-24 12:43:28 -0600

    Used fastScan instead of scanf, runtime dropped from 0.18s to 0.05s.

--- fast_io/enormous_input.cpp
+++ fast_io/enormous_input.cpp
@@ -18,17 +18,46 @@ Tested solution on https://www.codechef.com/problems/INTEST
 // I think it is not best practice to do so in work.
 #include <stdio.h>
 
+void fastScan(int &number) {
+    bool is_negative = false;
+    int c;
+    
+    number = 0;
+    
+    c = getchar();
+    if (c == '-') {
+        is_negative = true;
+        
+        c = getchar();
+    }
+    for (; (c >=48 && c <= 57); c=getchar()) {
+        number = number * 10 + c - 48;
+    }
+    if (is_negative) {
+        number *= -1;
+    }
+}
+
 int main() {
     int n, k, num, res = 0;
 
-    // scanf is faster than cin unless the synchronization between
-    // C and C++ is disabled, which can be done by ios_base::sync_with_stdio(false);
     scanf("%d %d", &n, &k);
+    getchar();  // To read \n
     for (int i=0; i<n; i++) {
-        scanf("%d", &num);
+        /*
+        THIS LINE IS THE MOST IMPORTANT
+
+        cin and scanf are the common solutions. scanf is faster than cin unless
+        the synchronization between C and C++ is disabled, which can be done by
+        ios_base::sync_with_stdio(false).
+
+        cin and scanf can both achieve 0.15-0.18s for the test cases on CodeChef.
+
+        fastScan significantly reduced the runtime to 0.05s.
+        */
+        fastScan(num);
         if (num % k == 0) {
-          res++;
-            
+            res++;
         }
     }
     printf("%d", res);
